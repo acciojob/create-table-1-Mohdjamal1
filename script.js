7a5c4006c5c0c7f1143efb15b57fe07b75844030
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2023-05-19 09:56:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+    const myTable = documen.querySelector('#sampleTable');
+	
+	let tr = document.createElement('tr');
+	const td1 = document.createElement('td');
+	const td2 = document.createElement('td');
+	td1.innerText = "New Cell1";
+	td2.innerText = "New Cell2";
+	tr.append(td1);
+	tr.append(td2);
+	myTable.append(tr);
 }
+  
